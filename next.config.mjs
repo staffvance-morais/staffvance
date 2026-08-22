7b657a23c1620a87d92e7c423c653f4e5cec4364
disnou <98349979+Disnou@users.noreply.github.com>
2026-08-22 08:31:26 -0300

    fix: desativar middleware temp e add headers de seguranca no next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "X-Frame-Options", value: "SAMEORIGIN" }, // Bloqueia Clickjacking
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" }, // Protege o histórico de navegação
+          { key: "Permissions-Policy", value: "camera=(), microphone=(), geolocation=()" }, // Bloqueia recursos não usados
+        ],
+      },
+    ];
+  },
 };
-
 export default nextConfig;
